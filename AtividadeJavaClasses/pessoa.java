12245a29cb7c88cf3ad37d17422d576d81dcb7c1
Wende(ll)veloper <110984453+Wen-developer@users.noreply.github.com>
2023-10-20 22:01:49 -0300

    Update pessoa.java
    get and set

--- AtividadeJavaClasses/pessoa.java
+++ AtividadeJavaClasses/pessoa.java
@@ -19,7 +19,69 @@ public class Piloto {
         this.vitorias = vitorias;
     }
 
-    // Getters e setters...
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(String equipe) {
+        this.equipe = equipe;
+    }
+
+    public String getCarro() {
+        return carro;
+    }
+
+    public void setCarro(String carro) {
+        this.carro = carro;
+    }
+
+    public int getPosicaoCorrida() {
+        return posicaoCorrida;
+    }
+
+    public void setPosicaoCorrida(int posicaoCorrida) {
+        this.posicaoCorrida = posicaoCorrida;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getCorridasDisputadas() {
+        return corridasDisputadas;
+    }
+
+    public void setCorridasDisputadas(int corridasDisputadas) {
+        this.corridasDisputadas = corridasDisputadas;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
 
     @Override
     public String toString() {
